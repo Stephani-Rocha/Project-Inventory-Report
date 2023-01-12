b58c72f08c7b982f46382192ca00947a2a07b42f
Stephani Rocha <gomes.stephani19@gmail.com>
2023-01-12 15:31:56 -0300

    feat(Versão simplificada do relatório): Cria método "generate" que gera a versão simplificada do relatório de estoque.

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,21 @@
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(file):
+        manufacturing_dates = [item["data_de_fabricacao"] for item in file]
+        earliest_manufacturing_date = sorted(manufacturing_dates)[0]
+
+        expiration_dates = [date["data_de_validade"] for date in file]
+        closest_expiration_dates = sorted(expiration_dates)[0]
+
+        names_company = [company["nome_da_empresa"] for company in file]
+        company_plus_product = Counter(names_company).most_common(1)[0][0]
+
+        return (
+            "Data de fabricação mais antiga: "
+            f"{earliest_manufacturing_date}\n"
+            f"Data de validade mais próxima: {closest_expiration_dates}\n"
+            f"Empresa com mais produtos: {company_plus_product}"
+        )
