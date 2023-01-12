92a1a7ecc2fc352b201c6e982e0b39bbdf1cf240
Stephani Rocha <gomes.stephani19@gmail.com>
2023-01-12 16:28:49 -0300

    feat(Versão completa do relatório): Implementa a classe CompleteReport que herda de SimpleReport e sobrescreve método generate, especializando seu comportamento.

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,14 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(file):
+        report = super(CompleteReport, CompleteReport).generate(file)
+        names_company = [company["nome_da_empresa"] for company in file]
+        companys_and_products = Counter(names_company)
+        report += "\nProdutos estocados por empresa:\n"
+        for company, qtd_product in companys_and_products.items():
+            report += f"- {company}: {qtd_product}\n"
+        return report
