75415d20ea30a9990a69ab5490a6cff6451fd390
Stephani Rocha <gomes.stephani19@gmail.com>
2023-01-18 10:00:41 -0300

    feat(Importa arquivo CSV): Implementa o método import_data, que importa arquivo CSV

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,19 @@
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+import csv
+
+
+class Inventory:
+    @staticmethod
+    def import_data(path, type_report):
+        with open(path, encoding='utf-8') as file:
+            if path.endswith(".csv"):
+                data = csv.DictReader(file)
+                data_list = list(data)
+
+        if type_report == "simples":
+            simple_report = SimpleReport.generate(data_list)
+            return simple_report
+        if type_report == "completo":
+            complete_report = CompleteReport.generate(data_list)
+            return complete_report
